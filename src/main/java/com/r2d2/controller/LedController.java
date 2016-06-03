f5ee9f61a5e21deb4664ad5b407b700581dfc98a
claudiogustavogomez <claudiogustavogomez@hotmail.com>
2016-06-02 23:36:35 -0300

    upload files to master branch

---
+++ src/main/java/com/r2d2/controller/LedController.java
@@ -0,0 +1,31 @@
+package com.r2d2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+@RestController
+public class LedController {
+	
+	private static GpioPinDigitalOutput pin;
+	
+	@RequestMapping("/")
+	public String saludo() {
+		return "Hello Artur !!";
+	}
+	
+	public String led(){
+		if(pin == null){
+			GpioController gpio = GpioFactory.getInstance();
+			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "mi led", PinState.LOW);
+		}
+		pin.toggle();
+		return "OK";
+	}
+	
+}
