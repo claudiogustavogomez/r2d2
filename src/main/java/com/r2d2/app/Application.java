941d8a1c9ffc4d2dc32154297ef8ed880ac39a63
claudiogustavogomez <claudiogustavogomez@hotmail.com>
2016-06-14 23:42:21 -0300

    refactor

--- src/main/java/com/r2d2/app/Application.java
+++ src/main/java/com/r2d2/app/Application.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
