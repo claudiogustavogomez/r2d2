99164aeff6300ea608599fabf69c40499b8dea7d
claudiogustavogomez <claudiogustavogomez@hotmail.com>
2016-06-17 01:01:13 -0300

    refactoring

--- src/main/java/com/r2d2/app/Application.java
+++ src/main/java/com/r2d2/app/Application.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.r2d2.app.controller.LedController;
+
 @SpringBootApplication
+@ComponentScan(basePackageClasses=LedController.class)
 public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
